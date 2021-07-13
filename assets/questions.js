1d7dd7a48d1f09b5c14f6bd9b5415780cabb4a17
Caitlin Tighe <caitighe@gmail.com>
2021-07-13 19:12:10 -0400

    created questions in the questions.js file

---
+++ assets/questions.js
@@ -0,0 +1,57 @@
+
+var questions = [
+    {
+        title: "What year was javascript introduced?",
+        choices: [1942, 2001, 1995, 2004],
+        answer: 1995
+    },
+
+    {
+        title: "What method is used to join two or more arrays?",
+        choices: [".concat()", ".splice()", ".push()", ".map()"],
+        answer: ".concat()"
+    },
+
+    {
+        title: "Javascript is a ___-side programming language", 
+        choices: ["Client", "Server", "Both", "None"],
+        answer: "Both"
+    },
+
+    {
+        title: "How do you find the minimum of x and y using JS?",
+        choices: ["min(x,y)", "Math.min(x,y)", "Math.min(xy)", "min(xy)"],
+        answer: "Math.min(x,y)"
+    },
+
+    {
+        title: "What tag do you use in HTML to link your javascript?",
+        choices: ["<javascript>", "<js>", "<script>", "<JAVASCRIPT>"],
+        answer: "<script>"
+    },
+
+    {
+        title: "Which Of The Dialog Box Display a Message And a Data Entry Field?",
+        choices: ["Alert()", "Prompt()", "Confirm()", "Msg()"],
+        answer: "Prompt()"
+    },
+
+    {
+        title: "Which function is used to parse a string to integer?",
+        choices: ["Integer.Parse", "Int.Parse", "Parse.Int", "Parse"],
+        answer: "Int.Parse"
+    },
+
+    {
+        title: "If a button is clicked which event handler is invoked?",
+        choices: ["OnSubmit()", "OnLoad()", "IsPostBack()", "Onclick()"],
+        answer: "Onclick()"
+    },
+
+    {
+        title: "A function associated with an object is called",
+        choices: ["Function", "Method", "Link", "None"],
+        answer: "Method"
+    }
+
+];
