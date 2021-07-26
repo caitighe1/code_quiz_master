f0d6138978388b7bced280dfb99d9a323e1b2432
Caitlin Tighe <caitighe@gmail.com>
2021-07-25 21:47:29 -0400

    made a document.getElement for all elements

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,27 @@ var timerID;
 var score = 0;
 
 //grabbing all DOM elements
+var navbarEl = document.getElementById("navbar");
+var highScoresEl = document.getElementById("highScores");
 var timerEl = document.getElementById("timer");
-var scoresEl = document.getElementById("scores");
+var mainEl = document.getElementById("main");
+var headerEl = document.getElementById("title")
+var rulesEl = document.getElementById("rules");
 var startEl = document.getElementById("start");
-var questionsEl = document.getElementById("questions");
-var scoreEl = document.getElementById("score");
+var questiondivEl = document.getElementById("questionDiv");
+var questionTitleEl = document.getElementById("questionTitle");
+var choicesEl = document.getElementById("choices");
+var answerDiv = document.getElementById("answerDiv");
+var answerEl = document.getElementById("result");
+var scoreDivEl = document.getElementById("scoreDiv");
+var finalResultEl = document.getElementById("finalResult");
+var finalScoreEl = document.getElementById("finalScore");
+var enterInfoEl = document.getElementById("enterInfo");
+var finalSubmit = document.getElementById("finalSubmit");
+
+
+
+
 
 
 
