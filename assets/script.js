7a78e2770aff3ff60983aa0f332bd473645d7da2
Caitlin Tighe <caitighe@gmail.com>
2021-07-25 21:59:30 -0400

    created event listener to start game and function to start game

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,11 @@ var score = 0;
 var navbarEl = document.getElementById("navbar");
 var highScoresEl = document.getElementById("highScores");
 var timerEl = document.getElementById("timer");
-var mainEl = document.getElementById("main");
+var mainEl = document.querySelector("main");
 var headerEl = document.getElementById("title")
 var rulesEl = document.getElementById("rules");
-var startEl = document.getElementById("start");
-var questiondivEl = document.getElementById("questionDiv");
+var startBtnEl = document.getElementById("startBtn");
+var questiondivEl = document.querySelector("quiz");
 var questionTitleEl = document.getElementById("questionTitle");
 var choicesEl = document.getElementById("choices");
 var answerDiv = document.getElementById("answerDiv");
@@ -25,9 +25,25 @@ var enterInfoEl = document.getElementById("enterInfo");
 var finalSubmit = document.getElementById("finalSubmit");
 
 
+//function to start quiz
+function startQuiz() {
+    //hide main div
+    mainEl.setAttribute("class", "hide");
+    
+    //call for questions
+    startQuiz.setAttribute("class", "show");
+    
+    //start timer
+    timerId = setInterval(timer, 1000);
+
+    //show start time
+    timerEl.textContent = time;
+
+    getQuestions();
+}
 
 
-
-
+//to start quiz
+startBtnEl.onclick = startQuiz;
 
 
