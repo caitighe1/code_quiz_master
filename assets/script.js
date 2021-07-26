ec6ea1a96970187fb2bdbbdea7645cd20b4f093f
Caitlin Tighe <caitighe@gmail.com>
2021-07-25 22:18:37 -0400

    created a timer function and a function that loops over array of questions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 //global variables for keeping track of time and questions
 
-var currentQuestion = 0;
+var currentQuestionIndex = 0;
 var time = (questions.length) * 15;
 var timerID;
 var score = 0;
@@ -14,7 +14,7 @@ var headerEl = document.getElementById("title")
 var rulesEl = document.getElementById("rules");
 var startBtnEl = document.getElementById("startBtn");
 var questiondivEl = document.querySelector("quiz");
-var questionTitleEl = document.getElementById("questionTitle");
+
 var choicesEl = document.getElementById("choices");
 var answerDiv = document.getElementById("answerDiv");
 var answerEl = document.getElementById("result");
@@ -31,15 +31,56 @@ function startQuiz() {
     mainEl.setAttribute("class", "hide");
     
     //call for questions
-    startQuiz.setAttribute("class", "show");
+    quiz.setAttribute("class", "show");
     
     //start timer
-    timerId = setInterval(timer, 1000);
+    timerId = setInterval(tick, 1000);
 
     //show start time
     timerEl.textContent = time;
 
     getQuestions();
+}
+
+//timer function
+function tick() {
+    //update time
+    time--;
+    timerEl.textContent = time;
+
+    //if user runs out of time
+    if (time <=0) {
+        quizEnd();
+    }
+}
+
+//grab questions from array
+function getQuestion() {
+    //grab current question
+    var currentQuestion = questions[currentQuestionIndex];
+
+    //update title with current question
+    var questionTitleEl = document.getElementById("questionTitle"); 
+    questionTitleEl.textContent = currentQuestion.title;
+
+    //clears old choices
+    choicesEl.innerHTML = "";
+
+    //loop over choices
+    currentQuestion.choices.forEach(function(choice, i) {
+        //create button for new choices
+        var choiceBtns = document.createElement("button");
+        choiceBtns.setAttribute("class", "choices");
+        choiceBtns.setAttribute("value", choice);
+
+        choiceBtns.textContent = i + 1 + "." + choice;
+
+        //add event listener to buttons
+        choiceBtns.onclick = questionClick;
+
+        //display on page
+        choicesEl.appendChild(choiceBtns);
+    });
 }
 
 
