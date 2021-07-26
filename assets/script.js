1d5fa84c30cd566d4df85b32efb4d26b88e9a996
Caitlin Tighe <caitighe@gmail.com>
2021-07-26 10:14:59 -0400

    matching selectors

--- assets/script.js
+++ assets/script.js
@@ -6,23 +6,18 @@ var timerID;
 
 
 //grabbing all DOM elements
-var timerEl = document.querySelector("#time");
-var highScore = document.querySelector("#high-score");
-var startBtn = document.querySelector("#startBtn");
-
-var header = document.querySelector("#header");
-
-var questionsEl = document.querySelector("#questionsDiv");
-var questionTitle = document.querySelector("#question-title");
-var choicesEl = document.querySelector("choices");
-
-var answerDiv = document.querySelector("#answerDiv")
-
+var timeEl = document.querySelector("#time");
+var startBtn = document.querySelector("#startButton");
+var submitBtn = document.querySelector("#submit-button");
+var titleScreen = document.querySelector("#title-section");
+var quizScreen = document.querySelector("#quiz-section");
+var highScoreScreen = document.querySelector("#highscore-section");
+var highScoreDisplay = document.querySelector("#highscore-display-section");
+var initialsEL = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
 
-//var scoreDivEl = document.querySelector("#scoreDiv");
-//var finalScoreEl = document.querySelector("#final-score");
-var finalSubmit = document.querySelector("final-submit")
+var questionsEl = document.querySelector("question");
+var choicesEl = document.querySelector("#choices");
 
 
 
